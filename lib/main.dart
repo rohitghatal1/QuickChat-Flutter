a3c7441ca916431d5e8f76868fc895aea3818756
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-04-17 20:43:55 +0545

    added home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firt_flutter_app/Screens/HomeScreen.dart';
 import 'package:firt_flutter_app/userLogin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      primaryColor: Color(0xFF1976D2),
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        secondary: Color(0xFF64B5F6),
+      ),
+    ),
+    home: Homescreen(),
     debugShowCheckedModeBanner: false,
     initialRoute: 'login',
     routes: {
